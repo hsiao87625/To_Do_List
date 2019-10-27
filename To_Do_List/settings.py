e286548e61fa90b96fc0845cd314cfeda336da34
Your Name <you@example.com>
2019-10-27 22:56:17 +0800

    Third Days

--- To_Do_List/settings.py
+++ To_Do_List/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -25,7 +26,7 @@ SECRET_KEY = '^p*d1d^)a44hd6252j!x7)_s2@!l$z5)i*@=o#!bnd69-hg7dj'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [https://hsiao-app2.herokuapp.com/]
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -119,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+django_heroku.settings(locals())
