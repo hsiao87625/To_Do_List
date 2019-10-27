9b628ef23cb210d699f794f49f8dba4f3106e92c
Your Name <you@example.com>
2019-10-27 22:50:00 +0800

    Third Days

--- To_Do_List/settings.py
+++ To_Do_List/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '^p*d1d^)a44hd6252j!x7)_s2@!l$z5)i*@=o#!bnd69-hg7dj'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [https://hsiao-app2.herokuapp.com/]
 
 
 # Application definition
